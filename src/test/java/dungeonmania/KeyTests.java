499c0df8291de91b349bdfed44c049a44c5000f8
David Peng <david.peng050301@gmail.com>
2022-07-07 19:28:56 +1000

    added key tests

---
+++ src/test/java/dungeonmania/KeyTests.java
@@ -0,0 +1,56 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class KeyTests {
+    @Test
+    @DisplayName("Test player can use a key to open and walk through a door")
+    public void useKeyWalkThroughOpenDoor() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_DoorsKeysTest_useKeyWalkThroughOpenDoor", "c_DoorsKeysTest_useKeyWalkThroughOpenDoor");
+
+        // pick up key
+        res = dmc.tick(Direction.RIGHT);
+        Position pos = getEntities(res, "player").get(0).getPosition();
+        assertEquals(1, getInventory(res, "key").size());
+
+        // walk through door and check key is gone
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(0, getInventory(res, "key").size());
+        assertNotEquals(pos, getEntities(res, "player").get(0).getPosition());
+    }
+
+    @Test
+    @DisplayName("Test player can't use wrong key to open and walk through a door")
+    public void cantUseWrongKey() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_DoorsKeysTest_useKeyWalkThroughOpenDoor", "c_DoorsKeysTest_cantUseKey");
+
+        // pick up key
+        res = dmc.tick(Direction.RIGHT);
+        Position pos = getEntities(res, "player").get(0).getPosition();
+        assertEquals(1, getInventory(res, "key").size());
+
+        // try to walk through door and check key is still there and player has not moved
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, getInventory(res, "key").size());
+        assertEquals(pos, getEntities(res, "player").get(0).getPosition());
+    }
+}
