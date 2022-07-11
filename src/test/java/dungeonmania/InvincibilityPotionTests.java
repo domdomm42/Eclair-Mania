f11cbcc7158f100745a445d28c0f1705af474df8
David Peng <david.peng050301@gmail.com>
2022-07-11 17:39:54 +1000

    added all potion tests

---
+++ src/test/java/dungeonmania/InvincibilityPotionTests.java
@@ -0,0 +1,123 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static dungeonmania.TestUtils.getInventory;
+
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.ItemResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class InvincibilityPotionTests {
+    private static DungeonResponse potionPickupAndUseMercenarySequence(DungeonManiaController controller, String configFile) throws InvalidActionException, IllegalArgumentException {
+        /*
+         *  exit   wall  wall  wall wall
+         * player  [IP]  [  ]  merc [  ] wall
+         *  wall   wall  wall  wall wall
+         */
+        DungeonResponse initialResponse = controller.newGame("d_battleTest_basicMercenaryInvincibilityPotion", configFile);
+        int mercenaryCount = countEntityOfType(initialResponse, "mercenary");
+        
+        assertEquals(1, countEntityOfType(initialResponse, "player"));
+        assertEquals(1, mercenaryCount);
+        DungeonResponse potionPickupResponse = controller.tick(Direction.RIGHT);
+        
+        EntityResponse actualPlayer = getPlayer(potionPickupResponse).get();
+        EntityResponse expectedPlayer = new EntityResponse(actualPlayer.getId(), actualPlayer.getType(), new Position(1, 1), false);
+
+        assertEquals(expectedPlayer, actualPlayer);
+
+        EntityResponse actualMercenary = getEntities(potionPickupResponse, "mercenary").get(0);
+        EntityResponse expectedMercenary = new EntityResponse(actualMercenary.getId(), actualMercenary.getType(), new Position(2, 1), false);
+
+        assertEquals(expectedMercenary, actualMercenary);
+
+        ItemResponse potion = getInventory(potionPickupResponse, "invincibility_potion").get(0);
+        DungeonResponse current = controller.tick(potion.getId());
+
+        actualPlayer = getPlayer(current).get();
+        expectedPlayer = new EntityResponse(actualPlayer.getId(), actualPlayer.getType(), new Position(1, 1), false);
+
+        assertEquals(expectedPlayer, actualPlayer);
+
+        actualMercenary = getEntities(current, "mercenary").get(0);
+        expectedMercenary = new EntityResponse(actualMercenary.getId(), actualMercenary.getType(), new Position(3, 1), false);
+
+        assertEquals(expectedMercenary, actualMercenary);
+
+        current = controller.tick(Direction.RIGHT);
+
+        actualPlayer = getPlayer(current).get();
+        expectedPlayer = new EntityResponse(actualPlayer.getId(), actualPlayer.getType(), new Position(2, 1), false);
+
+        assertEquals(expectedPlayer, actualPlayer);
+
+        actualMercenary = getEntities(current, "mercenary").get(0);
+        expectedMercenary = new EntityResponse(actualMercenary.getId(), actualMercenary.getType(), new Position(4, 1), false);
+
+        assertEquals(expectedMercenary, actualMercenary);
+        
+        return current;
+    }
+
+    private void assertBattleCalculations(String enemyType, BattleResponse battle, boolean enemyDies, String configFilePath) {
+        List<RoundResponse> rounds = battle.getRounds();
+        double playerHealth = Double.parseDouble(getValueFromConfigFile("player_health", configFilePath));
+        double enemyHealth = Double.parseDouble(getValueFromConfigFile(enemyType + "_attack", configFilePath));
+
+        for (RoundResponse round : rounds) {
+            enemyHealth -= round.getDeltaEnemyHealth();
+            playerHealth -= round.getDeltaCharacterHealth();
+        }
+
+        if (enemyDies) {
+            assertTrue(enemyHealth <= 0);
+        } else {
+            assertTrue(playerHealth <= 0);
+        }
+    }
+
+    @Test
+    @DisplayName("Test basic battle calculations - mercenary - player loses except player is invincible")
+    public void testInvincibilityMovement() throws InvalidActionException {
+       DungeonManiaController controller = new DungeonManiaController();
+       potionPickupAndUseMercenarySequence(controller, "c_battleTests_basicMercenaryPlayerDies");
+    }
+
+    @Test
+    @DisplayName("Test basic battle calculations - mercenary - player loses except player is invincible")
+    public void testHealthBelowZeroMercenary() throws InvalidActionException {
+       DungeonManiaController controller = new DungeonManiaController();
+       potionPickupAndUseMercenarySequence(controller, "c_battleTests_basicMercenaryPlayerDies");
+       DungeonResponse current = controller.tick(Direction.RIGHT);
+
+       EntityResponse actualPlayer = getPlayer(current).get();
+       EntityResponse expectedPlayer = new EntityResponse(actualPlayer.getId(), actualPlayer.getType(), new Position(3, 1), false);
+
+       assertEquals(expectedPlayer, actualPlayer);
+
+       EntityResponse actualMercenary = getEntities(current, "mercenary").get(0);
+       EntityResponse expectedMercenary = new EntityResponse(actualMercenary.getId(), actualMercenary.getType(), new Position(4, 1), false);
+
+       assertEquals(expectedMercenary, actualMercenary);
+
+       DungeonResponse postBattleResponse = controller.tick(Direction.RIGHT);
+       BattleResponse battle = postBattleResponse.getBattles().get(0);
+       assertBattleCalculations("mercenary", battle, true, "c_battleTests_basicMercenaryPlayerDies");
+    }
+}
