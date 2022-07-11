0cf978f129c98fa06cac781552cd817d28bf4df2
z5311637 <z5311637@unsw.edu.au>
2022-07-11 19:33:19 +1000

    added more mercenary tests

--- src/test/java/dungeonmania/MercenaryTests.java
+++ src/test/java/dungeonmania/MercenaryTests.java
@@ -1,6 +1,7 @@
 package dungeonmania;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -26,6 +27,7 @@ import dungeonmania.response.models.RoundResponse;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
+import dungeonmania.exceptions.InvalidActionException;;
 
 public class MercenaryTests {
     @Test
@@ -104,18 +106,71 @@ public class MercenaryTests {
         
     }
 
+    
     @DisplayName("Test that a mercenary can be bribed in a certain radius")
     public void testMercenaryBribe() {
-        
+        /*
+         *  exit   
+         * player  trea  trea  trea  [  ]  [  ]  [  ]  [Merc]
+         * 
+         * bribe amount 3 radius = 1
+         *                          
+         */                     
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_mercenaryTest_bribeMercenary", "c_mercenaryTest_bribeMercenary");
+        EntityResponse initMercenary = getEntities(initDungonRes, "mercenary").get(0);
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        // Get expected positions after 3 movements to the right
+        // Mercenary will be interactable when next to each other
+        EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(4, 2), false);
+        EntityResponse expectedMercenary = new EntityResponse(initMercenary.getId(), initMercenary.getType(), new Position(5, 2), true);
+
+        // Move player to right 3 times
+        dmc.tick(Direction.RIGHT);
+        dmc.tick(Direction.RIGHT);
+        DungeonResponse actualDungonRes = dmc.tick(Direction.RIGHT);
+
+        // Get actual positions of Mercenary and Player
+        EntityResponse actualMercenary = getEntities(actualDungonRes, "mercenary").get(0);
+        EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+
+        // Assert position of player and mercenary
+        assertEquals(expectedPlayer, actualPlayer);
+        assertEquals(expectedMercenary, actualMercenary);
+
+        // Assert that player has 3 treasure in the inventory
+        assertEquals(3, getInventory(actualDungonRes, "treasure").size());
+
+        // Player bribes mercenary
+        String mercenaryId = getEntities(actualDungonRes, "mercenary").get(0).getId();
+        actualDungonRes =  assertDoesNotThrow(() ->  dmc.interact(mercenaryId));
+
+        // Assert that treasure is used
+        assertEquals(0, getInventory(actualDungonRes, "treasure").size());
+
     }
 
-    @DisplayName("Test a mercenary isn't automatically bribed when it is in the radius") 
+    @DisplayName("Test a mercenary can't be bribed outside radius") 
     public void testMercenaryAutomaticBribeCase() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_mercenaryTest_bribeMercenary", "c_mercenaryTest_bribeMercenary");
+
+        // Player attempts to bribes mercenary outside of radius
+        String mercenaryId = getEntities(initDungonRes, "mercenary").get(0).getId();
+        assertThrows(InvalidActionException.class, () ->  dmc.interact(mercenaryId));
+        
     }
 
 
     @DisplayName("Test a Mercenary Ally Movement, that it follows player correctly")
     public void testMercenaryAllyMovement() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_mercenaryTest_bribeMercenary", "c_mercenaryTest_bribeMercenary");
+        EntityResponse initMercenary = getEntities(initDungonRes, "mercenary").get(0);
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        
 
     }
 
