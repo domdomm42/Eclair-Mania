ca8900717b8a5c2df348a234b086fca8f4923f71
Rupin <z5311637@unsw.edu.au>
2022-07-14 19:39:17 +1000

    added entity

--- src/main/java/dungeonmania/Entities/Entity.java
+++ src/main/java/dungeonmania/Entities/Entity.java
@@ -1,5 +1,48 @@
 package dungeonmania.Entities;
 
 public class Entity {
+    private String id, type;
+    private Position position;
+    private boolean isInteractable = false;
+    
+    public Entity(String id, String type, Position position, boolean isInteractable) {
+        this.id = id;
+        this.type = type;
+        this.position = position;
+        this.isInteractable = isInteractable;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public boolean getIsInteractable() {
+        return isInteractable;
+    }
+
+    public void setInteractable(boolean isInteractable) {
+        this.isInteractable = isInteractable;
+    }
+
     
 }
