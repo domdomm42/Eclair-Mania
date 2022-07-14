454a931d518d5d418a01b29c1d76b08650d7aa5d
Rupin <z5311637@unsw.edu.au>
2022-07-14 19:58:40 +1000

    created Enemy Classes

--- src/main/java/dungeonmania/Entities/MovingEntities/Enemies/Enemy.java
+++ src/main/java/dungeonmania/Entities/MovingEntities/Enemies/Enemy.java
@@ -1,5 +1,12 @@
 package dungeonmania.Entities.MovingEntities.Enemies;
 
-public class Enemy {
+import dungeonmania.Entities.MovingEntities.MovingEntity;
+import dungeonmania.util.Position;
+
+public class Enemy extends MovingEntity {
+
+    public Enemy(String id, String type, Position position, int health, boolean isInteractable) {
+        super(id, type, position, health, isInteractable);
+    }
     
 }
---
+++ src/main/java/dungeonmania/Entities/MovingEntities/Enemies/ZombieToastSpawner.java
@@ -0,0 +1,11 @@
+package dungeonmania.Entities.MovingEntities.Enemies;
+
+import dungeonmania.util.Position;
+
+public class ZombieToastSpawner extends Enemy{
+
+    public ZombieToastSpawner(String id, String type, Position position, int health, boolean isInteractable) {
+        super(id, type, position, health, isInteractable);
+    }
+    
+}
