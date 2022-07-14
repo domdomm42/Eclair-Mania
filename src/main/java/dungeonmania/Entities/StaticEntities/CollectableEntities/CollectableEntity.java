e2c7b34637b2d3827520d0b75cfc310319fb3708
Lim Oudom <z5329509@unsw.edu.au>
2022-07-15 00:30:45 +1000

    fixed more file location

--- src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/CollectableEntity.java
+++ src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/CollectableEntity.java
@@ -1,7 +1,13 @@
 package dungeonmania.Entities.StaticEntities.CollectableEntities;
 
 import dungeonmania.Entities.StaticEntities.StaticEntity;
+import dungeonmania.util.Position;
 
 public abstract class CollectableEntity extends StaticEntity {
+
+    public CollectableEntity(Position position, String id, String type) {
+        super(position, id, type);
+        //TODO Auto-generated constructor stub
+    }
     
 }
--- src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/Potions/Potion.java
+++ src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/Potions/Potion.java
@@ -1,6 +1,6 @@
 package dungeonmania.Entities.StaticEntities.CollectableEntities.Potions;
 
-import dungeonmania.Entities.StaticEntities.Collectables.Usable;
+import dungeonmania.Entities.StaticEntities.CollectableEntities.Usable;
 
 public abstract class Potion extends Usable {
     
