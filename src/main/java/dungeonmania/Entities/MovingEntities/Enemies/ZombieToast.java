ca410adf874081e911c513942ba6af593a14200f
z5311637 <z5311637@unsw.edu.au>
2022-07-15 23:33:02 +1000

    fixed name of zombie toast class

---
+++ src/main/java/dungeonmania/Entities/MovingEntities/Enemies/ZombieToast.java
@@ -0,0 +1,12 @@
+package dungeonmania.Entities.MovingEntities.Enemies;
+
+import dungeonmania.Entities.MovingEntities.MovementStrategy;
+import dungeonmania.util.Position;
+
+public class ZombieToast extends Enemy {
+
+    public ZombieToast(String id, String type, Position position, int health, boolean isInteractable, MovementStrategy movementStrategy, int attack) {
+        super(id, type, position, health, isInteractable, movementStrategy, health);
+    }
+    
+}
