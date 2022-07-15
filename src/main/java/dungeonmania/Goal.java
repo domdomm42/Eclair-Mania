051289ffac4e29e587429872aa328d8c4c3be9eb
Lim Oudom <z5329509@unsw.edu.au>
2022-07-15 22:42:06 +1000

    started goals

--- src/main/java/dungeonmania/Goal.java
+++ src/main/java/dungeonmania/Goal.java
@@ -3,9 +3,12 @@ package dungeonmania;
 import org.json.JSONObject;
 
 public class Goal {
-    
+    private boolean isComplex;
+    private ArrayList<Goal> goals;
+    private boolean isAnd;
 
     public Goal(JSONObject goalJson) {
+
 
     }
 }
