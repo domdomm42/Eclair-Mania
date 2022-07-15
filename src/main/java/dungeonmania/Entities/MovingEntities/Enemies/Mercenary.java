a7c888eb827b36d1f1bd0318329d7def6d38d200
z5311637 <z5311637@unsw.edu.au>
2022-07-15 23:25:37 +1000

    Created spider movement strategy

--- src/main/java/dungeonmania/Entities/MovingEntities/Enemies/Mercenary.java
+++ src/main/java/dungeonmania/Entities/MovingEntities/Enemies/Mercenary.java
@@ -1,13 +1,13 @@
 package dungeonmania.Entities.MovingEntities.Enemies;
 
+import dungeonmania.Entities.MovingEntities.MovementStrategy;
 import dungeonmania.util.Position;
 
 public class Mercenary extends Enemy {
-    private boolean isAlly = false;
     private int bribePrice;
     
-    public Mercenary(String id, String type, Position position, int health, boolean isInteractable, int bribePrice) {
-        super(id, type, position, health, isInteractable);
+    public Mercenary(String id, String type, Position position, int health, boolean isInteractable, MovementStrategy movementStrategy, int attack, int bribePrice) {
+        super(id, type, position, health, isInteractable, movementStrategy, attack);
         this.bribePrice = bribePrice;
     }
     
