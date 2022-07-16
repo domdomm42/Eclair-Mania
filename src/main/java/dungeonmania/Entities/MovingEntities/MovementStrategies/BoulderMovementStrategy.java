30c86afbad7b662cd8c54d598c2c732ad4a77489
David Peng <david.peng050301@gmail.com>
2022-07-16 23:22:38 +1000

    fixed boulder movement strategy and added isvalidmove

--- src/main/java/dungeonmania/Entities/MovingEntities/MovementStrategies/BoulderMovementStrategy.java
+++ src/main/java/dungeonmania/Entities/MovingEntities/MovementStrategies/BoulderMovementStrategy.java
@@ -2,24 +2,24 @@ package dungeonmania.Entities.MovingEntities.MovementStrategies;
 
 import java.util.List;
 
-// import dungeonmania.Battle;
 import dungeonmania.Dungeon;
 import dungeonmania.Entities.Entity;
 import dungeonmania.Entities.MovingEntities.MovementStrategy;
-import dungeonmania.Entities.StaticEntities.Boulder;
-// import dungeonmania.Entities.MovingEntities.Enemies.Enemy;
-// import dungeonmania.Entities.StaticEntities.CollectableEntities.CollectableEntity;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class BoulderMovementStrategy extends MovementStrategy {
     @Override
     public void move(Direction direction) {
-        Boulder boulder = (Boulder) getEntity();
-        Position requestedPosition = boulder.getPositionInDirection(direction);
-        List<Entity> entitiesOnPosition = Dungeon.getEntitiesAtPosition(requestedPosition);
-        if (entitiesOnPosition.stream().anyMatch(entity -> entity.getType() == "wall")) return;
-        if (entitiesOnPosition.stream().anyMatch(entity -> entity.getType() == "boulder")) return;
-        getEntity().setPosition(requestedPosition);
+        Position requestedPosition = getEntity().getPositionInDirection(direction);
+        if (isValidMove(requestedPosition)) getEntity().setPosition(requestedPosition);
+    }
+
+    @Override
+    public boolean isValidMove(Position position) {
+        List<Entity> entitiesOnPosition = Dungeon.getEntitiesAtPosition(position);
+        if (entitiesOnPosition.stream().anyMatch(entity -> entity.getType() == "wall")) return false;
+        if (entitiesOnPosition.stream().anyMatch(entity -> entity.getType() == "boulder")) return false;
+        return true;
     }
 }
