cdd739421a9063b9bce12e09280fae99b8df458a
Ridho Yusuf <ridho@Ridhos-MacBook-Air.local>
2022-07-17 13:53:08 +1000

    finalised integration test

--- src/test/java/dungeonmania/IntegrationTests.java
+++ src/test/java/dungeonmania/IntegrationTests.java
@@ -1,33 +1,19 @@
 package dungeonmania;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import static dungeonmania.TestUtils.getPlayer;
-import static dungeonmania.TestUtils.getEntities;
-import static dungeonmania.TestUtils.getInventory;
-import static dungeonmania.TestUtils.getGoals;
 import static dungeonmania.TestUtils.countEntityOfType;
-import static dungeonmania.TestUtils.getValueFromConfigFile;
-
-import java.util.ArrayList;
-import java.util.List;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getPlayer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import dungeonmania.response.models.BattleResponse;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.EntityResponse;
-import dungeonmania.response.models.RoundResponse;
 import dungeonmania.util.Direction;
-import dungeonmania.util.Position;
 
 
-public class FloorSwitchTests {
+public class IntegrationTests {
 
     @Test
     @DisplayName("Integration test 1")
@@ -35,7 +21,7 @@ public class FloorSwitchTests {
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "d_integrationTest_1");
         EntityResponse initPlayer = getPlayer(initDungonRes).get();
-        EntityResponse initSwitch = getEntities(initDungonRes, "switch").get(0);
+    
 
         // Pick up sword
         DungeonResponse res = dmc.tick(Direction.RIGHT);
@@ -73,39 +59,148 @@ public class FloorSwitchTests {
     @DisplayName("Integration test 2")
     public void integrationTest2() {
         DungeonManiaController dmc = new DungeonManiaController();
-        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "d_integrationTest_1");
+        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "d_integrationTest_2");
         EntityResponse initPlayer = getPlayer(initDungonRes).get();
-        EntityResponse initSwitch = getEntities(initDungonRes, "switch").get(0);
 
-        // Pick up sword
+        assertEquals(2, countEntityOfType(initDungonRes, "mercenary"));
+
+        // Pick up key
         DungeonResponse res = dmc.tick(Direction.RIGHT);
-        assertEquals(1, getInventory(res, "sword").size());
+        assertEquals(1, getInventory(res, "key").size());
         
         res = dmc.tick(Direction.RIGHT);
         res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.LEFT);
 
-        // Fight mercenary
+        // Fight mercenary 1
+        res = dmc.tick(Direction.LEFT);
+        assertEquals(1, countEntityOfType(res, "mercenary"));
+
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.DOWN);
+
+        // Pick up invincibility potion
+        res = dmc.tick(Direction.DOWN);
+
+        // Use potion
+        res = dmc.tick("invincibility_potion");
+
+        // Jump into portal
+        res = dmc.tick(Direction.DOWN);
+
+        // Fight mercenary 2
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
         res = dmc.tick(Direction.RIGHT);
         assertEquals(0, countEntityOfType(res, "mercenary"));
 
+        // Exit portal
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+
+        // Exit
+        res = dmc.tick(Direction.LEFT);
+    }
+
+    @Test
+    @DisplayName("Integration test 3")
+    public void integrationTest3() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "d_integrationTest_2");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        assertEquals(1, countEntityOfType(initDungonRes, "mercenary"));
+
+        // Pick up treasure and bomb
+        DungeonResponse res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, getInventory(res, "treasure").size());
+        assertEquals(1, getInventory(res, "bomb").size());
+        
+        // Fight mercenary
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(0, countEntityOfType(initDungonRes, "mercenary"));
+
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
         res = dmc.tick(Direction.DOWN);
-        res = dmc.tick(Direction.LEFT);
-        res = dmc.tick(Direction.LEFT);
-        res = dmc.tick(Direction.LEFT);
+
+        // Push boulder onto floor switch
+        res = dmc.tick(Direction.RIGHT);
+
+        // Pick up sword
         res = dmc.tick(Direction.DOWN);
+        assertEquals(1, getInventory(res, "sword").size());
+
+        // Drop bomb
+        res = dmc.tick("bomb");
+        assertEquals(0, getInventory(res, "bomb").size());
 
         // Pick up key
         res = dmc.tick(Direction.RIGHT);
         assertEquals(1, getInventory(res, "key").size());
 
-        // Enter portal
+        // Go through door
         res = dmc.tick(Direction.RIGHT);
+        assertEquals(0, countEntityOfType(res, "key"));
+
+        // Fight zombies
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+
+        // Go to exit
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+    }
+
+    @Test
+    @DisplayName("Integration test 4")
+    public void integrationTest4() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "d_integrationTest_2");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        assertEquals(2, countEntityOfType(initDungonRes, "mercenary"));
+
+        // Pick up 3 wood and fight mercenary
+        DungeonResponse res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(3, getInventory(res, "wood").size());
+        assertEquals(1, countEntityOfType(initDungonRes, "mercenary"));
+        
+        // Pick up 3 arrows
+        res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.LEFT);
+        res = dmc.tick(Direction.LEFT);
+        assertEquals(3, countEntityOfType(initDungonRes, "arrow"));
+
+        // Pick up treasure
+        res = dmc.tick(Direction.DOWN);
+        assertEquals(3, countEntityOfType(initDungonRes, "treasure"));
+
+        // Get key
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, countEntityOfType(initDungonRes, "key"));
 
         // Unlock door
         res = dmc.tick(Direction.RIGHT);
-        assertEquals(0, getInventory(res, "key").size());
+
+        // Fight mercenary
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(0, countEntityOfType(initDungonRes, "mercenary"));
 
         // Exit
+        res = dmc.tick(Direction.RIGHT);
         res = dmc.tick(Direction.RIGHT);
     }
 }
