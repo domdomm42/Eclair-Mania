2e49f5bb1c2ed16b6e41a8b15f0a10ad7fa290c9
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-07-20 07:35:11 +1000

    Milestone 2 Automarking

--- src/main/java/dungeonmania/Entities/MovingEntities/Enemies/Spawnable.java
+++
@@ -1,6 +0,0 @@
-package dungeonmania.Entities.MovingEntities.Enemies;
-
-public interface Spawnable {
-    public boolean isSpawnable();
-    public void spawn();
-}
--- src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/Arrow.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.Entities.StaticEntities.CollectableEntities;
-
-import dungeonmania.util.Position;
-
-public class Arrow extends CollectableEntity {
-    public Arrow(String id, Position position) {   
-        super(false, 1, position, id, "arrow");
-    }
-}
