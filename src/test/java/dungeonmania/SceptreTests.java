fae6db90e370fcb5041aba38267c2e37f0dd99a4
Ridho Yusuf <ridho@Ridhos-MacBook-Air.local>
2022-07-26 18:35:47 +1000

    implemented tests

--- src/test/java/dungeonmania/SceptreTests.java
+++ src/test/java/dungeonmania/SceptreTests.java
@@ -14,21 +14,105 @@ public class SceptreTests {
     
     @Test
     @DisplayName("Player builds Spectre from 1 wood + 1 key + 1 sun stone")
-    public void testMakeSceptre1() {
+    public void testMakeSceptre1() throws IllegalArgumentException, InvalidActionException {
+        DungeonManiaController dmc = new DungeonManiaController();
+        dmc.newGame("d_spectreTest_build", "c_spectreTest");
+
+        // pick up items
+        DungeonResponse res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(1, getInventory(res, "wood").size());
+        assertEquals(1, getInventory(res, "key").size());
+        assertEquals(1, getInventory(res, "sun_stone").size());
+
+        dmc.build("sceptre");
+        
+        assertEquals(0, getInventory(res, "wood").size());
+        assertEquals(0, getInventory(res, "key").size());
+        assertEquals(0, getInventory(res, "sun_stone").size());
+        assertEquals(1, getInventory(res, "sceptre").size());
     }
 
     @Test
     @DisplayName("Player builds Spectre from 1 wood + 1 treasure + 1 sun stone")
-    public void testMakeSceptre2() {
+    public void testMakeSceptre2() throws IllegalArgumentException, InvalidActionException {
+        DungeonManiaController dmc = new DungeonManiaController();
+        dmc.newGame("d_spectreTest_build", "c_spectreTest");
+
+        // pick up items
+        DungeonResponse res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(1, getInventory(res, "wood").size());
+        assertEquals(1, getInventory(res, "treasure").size());
+        assertEquals(1, getInventory(res, "sun_stone").size());
+
+        dmc.build("sceptre");
+        
+        assertEquals(0, getInventory(res, "wood").size());
+        assertEquals(0, getInventory(res, "treasure").size());
+        assertEquals(0, getInventory(res, "sun_stone").size());
+        assertEquals(1, getInventory(res, "sceptre").size());   
     }
 
     @Test
     @DisplayName("Player builds Spectre from 2 arrows + 1 treasure + 1 sun stone")
-    public void testMakeSceptre3() {
+    public void testMakeSceptre3() throws IllegalArgumentException, InvalidActionException {
+        DungeonManiaController dmc = new DungeonManiaController();
+        dmc.newGame("d_spectreTest_build", "c_spectreTest");
+
+        // pick up items
+        DungeonResponse res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(1, getInventory(res, "arrow").size());
+        assertEquals(1, getInventory(res, "arrow").size());
+        assertEquals(1, getInventory(res, "treasure").size());
+        assertEquals(1, getInventory(res, "sun_stone").size());
+
+        dmc.build("sceptre");
+        
+        assertEquals(0, getInventory(res, "arrow").size());
+        assertEquals(0, getInventory(res, "arrow").size());
+        assertEquals(0, getInventory(res, "treasure").size());
+        assertEquals(0, getInventory(res, "sun_stone").size());
+        assertEquals(1, getInventory(res, "sceptre").size());   
     }
 
     @Test
     @DisplayName("Player builds Spectre from 2 arrows + 1 key + 1 sun stone")
-    public void testMakeSceptre4() {
+    public void testMakeSceptre4() throws IllegalArgumentException, InvalidActionException {
+        DungeonManiaController dmc = new DungeonManiaController();
+        dmc.newGame("d_spectreTest_build", "c_spectreTest");
+
+        // pick up items
+        DungeonResponse res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.DOWN);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(1, getInventory(res, "arrow").size());
+        assertEquals(1, getInventory(res, "arrow").size());
+        assertEquals(1, getInventory(res, "key").size());
+        assertEquals(1, getInventory(res, "sun_stone").size());
+
+        dmc.build("sceptre");
+        
+        assertEquals(0, getInventory(res, "arrow").size());
+        assertEquals(0, getInventory(res, "arrow").size());
+        assertEquals(0, getInventory(res, "key").size());
+        assertEquals(0, getInventory(res, "sun_stone").size());
+        assertEquals(1, getInventory(res, "sceptre").size()); 
     }
 }
