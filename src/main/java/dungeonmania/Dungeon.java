13b52778c616ec6dedd5c821925d0b375de50051
Ridho Yusuf <ridho@Ridhos-MacBook-Air.local>
2022-07-30 03:12:57 +1000

    did dungeon builder

--- src/main/java/dungeonmania/Dungeon.java
+++ src/main/java/dungeonmania/Dungeon.java
@@ -336,25 +336,139 @@ public class Dungeon {
     // DUNGEON GENERATION
     public static void generateDungeon(int xStart, int yStart, int xEnd, int yEnd) {
         resetDungeon();
+        Dungeon.dungeonName = "maze";
 
-        boolean[][] mazeMap = generateRandomMaze(xStart, yStart, xEnd, yEnd);
+        int height = Math.abs(yEnd - yStart);
+        int width = Math.abs(xEnd - xStart);
 
-        // Dungeon.dungeonName = dungeonName;
-        // JsonObject dungeonJson = dungeonFileToJson(dungeonName);
-        // createEntities(dungeonJson);
-        // loadGoals(dungeonJson.getAsJsonObject("goal-condition"));
+        boolean[][] maze = generateRandomMaze(height, width);
+
+        JsonArray entities = new JsonArray();
+        
+        // Add player at START
+        JsonObject player = new JsonObject();
+        player.addProperty("type", "exit");
+        player.addProperty("x", xStart);
+        player.addProperty("y", yStart);
+        entities.add(player);
+        
+        // Add exit at END
+        JsonObject exit = new JsonObject();
+        exit.addProperty("type", "exit");
+        exit.addProperty("x", xEnd);
+        exit.addProperty("y", yEnd);
+        entities.add(exit);
+
+        // Create border of walls around maze
+        for (int x = xStart - 1; x <= xEnd; x++) {
+            for (int y = yStart - 1; y <= yEnd; y++) {
+                if (x == xStart - 1 || y == yStart - 1 || x == xEnd || y == yEnd) {
+                    JsonObject wall = new JsonObject();
+                    wall.addProperty("type", "wall");
+                    wall.addProperty("x", x);
+                    wall.addProperty("y", y);
+                    entities.add(wall);
+                }
+            }
+        }
+        
+        // Create maze
+        for (int x = xStart, i = 0; x < xEnd; x++, i++) {
+            for (int y = yStart, j = 0; y < yEnd; y++, j++) {
+                if (maze[i][j]) {
+                    JsonObject wall = new JsonObject();
+                    wall.addProperty("type", "wall");
+                    wall.addProperty("x", x);
+                    wall.addProperty("y", y);
+                    entities.add(wall); 
+                }
+            }
+        }
+
+        loadEntities(entities);
+    
+        // Create the only goal: exit
+        JsonObject exitGoal = new JsonObject();
+        exitGoal.addProperty("goal", "exit");
+
+        loadGoals(exitGoal);
     }
 
-    private static boolean[][] generateRandomMaze(int xStart, int yStart, int xEnd, int yEnd) {
-        boolean[][] arr = new boolean[xEnd - xStart][yEnd - xStart];
-        return arr;
+    private static boolean[][] generateRandomMaze(int height, int width) {
+        int boundaryYBottom = height - 1;
+        int boundaryXRight = width - 1;
+
+        Random r = new Random();
+
+        boolean[][] maze = new boolean[width][height];
+
+        maze[0][0] = true;
+
+        List<Position> options = getNeighbours(maze, new Position(0, 0), 2, true);
+
+        while (options.size() > 0) {
+            Position next = options.remove(r.nextInt(options.size()));
+
+            List<Position> neighbours = getNeighbours(maze, next, 2, false);
+            if (neighbours.size() != 0) {
+                Position neigbour = neighbours.get(r.nextInt(neighbours.size()));
+
+                maze[next.getX()][next.getY()] = true;
+                maze[(next.getX() + neigbour.getX()) / 2][(next.getY() + neigbour.getY()) / 2] = true;
+                maze[neigbour.getX()][neigbour.getY()] = true;
+            }
+
+            getNeighbours(maze, next, 2, true).forEach(pos -> options.add(pos));
+        }
+
+        if (!maze[boundaryXRight][boundaryYBottom]) {
+            maze[boundaryXRight][boundaryYBottom] = true;
+
+            List<Position> neighbours = new ArrayList<Position>();
+            neighbours.add(new Position(boundaryXRight - 1, boundaryYBottom));
+            neighbours.add(new Position(boundaryXRight, boundaryYBottom - 1));
+            if (neighbours.stream().anyMatch(p -> !maze[p.getX()][p.getY()])) {
+                Position neighbour = neighbours.get(r.nextInt(neighbours.size()));
+                maze[neighbour.getX()][neighbour.getY()] = true;
+            }
+        }
+
+        return maze;
+    }
+
+    // Returns a list of neighbours to a position which are:
+    // - a cardinally adjacent distance of 2 away
+    // - is a wall (false)
+    private static List<Position> getNeighbours(boolean[][] maze, Position pos, int dist, boolean areWalls) {
+        
+        List<Position> possibleNeigbours = new ArrayList<Position>();
+
+        // Add the four possible neighbours
+        possibleNeigbours.add(pos.translateBy(0, dist));
+        possibleNeigbours.add(pos.translateBy(0, -dist));
+        possibleNeigbours.add(pos.translateBy(dist, 0));
+        possibleNeigbours.add(pos.translateBy(-dist, 0));
+        
+        return possibleNeigbours.stream().filter(p -> p.getX() >= 0 && p.getX() < maze[0].length && p.getY() >= 0 && p.getY() < maze[0].length && ((areWalls) ? !maze[p.getX()][p.getY()] : maze[p.getX()][p.getY()])).collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        boolean[][] arr = generateRandomMaze(0, 0, 5, 5);
-        arr[0][0] = true;
+        int height = 21;
+        int width = 21;
+        boolean[][] arr = generateRandomMaze(height, width);
 
-        System.out.println(arr[0][0]);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+
+                if (arr[i][j]) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("O");
+                }
+                
+            }
+            System.out.println("");
+        }
     }
     
 }
