81c6100ea859af7b95212d291bb1e40f2a8f633b
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-08-06 22:20:33 +1000

    Milestone 3 automarking Results

--- src/main/java/dungeonmania/Entities/StaticEntities/CollectableEntities/TimeTurner.java
+++
@@ -1,9 +0,0 @@
-package dungeonmania.Entities.StaticEntities.CollectableEntities;
-
-import dungeonmania.util.Position;
-
-public class TimeTurner extends CollectableEntity{
-    public TimeTurner(Position position, String id) {
-        super(false, 1, position, id, "time_turner");
-    }
-}
